45f7a432601e060efa3e1f648cb76abdc918787b
hannahammonds <90658994+hannahammonds@users.noreply.github.com>
2021-11-15 20:10:52 -0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'to-plant';
+  @ViewChild ('f') toPlantForm: NgForm
+  defaultSeason = 'Spring'
+  plant = {
+    seedname: '',
+    amount: '',
+    weeks: '',
+    conditions: '',
+    season: '',
+  }
+submitted = false;
+  onSubmit(){
+    this.submitted = true;
+    this.plant.seedname = this.toPlantForm.value.seedname;
+    this.plant.amount = this.toPlantForm.value.amount;
+    this.plant.weeks = this.toPlantForm.value.weeks;
+    this.plant.conditions = this.toPlantForm.value.conditions;
+    this.plant.season = this.toPlantForm.value.season;
+    
+    this.toPlantForm.reset();
+    console.log(this.toPlantForm);
+  }
+}
