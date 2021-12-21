b0afa00eae4c30225b5872ae26bffebdd6a7a0fb
hannahammonds <90658994+hannahammonds@users.noreply.github.com>
2021-12-20 20:28:41 -0600

    auth service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,18 +10,33 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
   isLoginMode = true;
 
-  constructor() { }
-
+  constructor( private authService: AuthService) { }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm){
-    console.log(form.value);
-    form.reset();
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+  this.authService.signup(email, password)
+  form.reset();
+
   }
 
 }
+function email(email: any, password: any) {
+  throw new Error('Function not implemented.');
+}
+
+function password(email: (email: any, password: any) => void, password: any) {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,19 +3,25 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment'
 
 
-export class interface AuthResponse
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return
-    this.http.post(
+    return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key'+ environment.firebaseKey,
       {email: email,
       password: password,
-    returnSecureToken: true
+      returnSecureToken: true
   });
   }
 }
