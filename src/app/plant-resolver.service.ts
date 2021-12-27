2ed475fbf39f89c202ca7f3701a6b5c55884d0f3
hannahammonds <90658994+hannahammonds@users.noreply.github.com>
2021-12-27 11:46:54 -0600

    Update plant-resolver.service.ts

--- src/app/plant-resolver.service.ts
+++ src/app/plant-resolver.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ɵangular_packages_platform_browser_platform_browser_k } from '@angular/platform-browser';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { DataStorageService } from './data-storage.service';
 import { Plant } from './plant.service';
@@ -7,10 +8,16 @@ import { Plant } from './plant.service';
   providedIn: 'root'
 })
 export class PlantResolverService implements Resolve<Plant[]> {
+  plantService: any;
 
   constructor(private dataStorageService: DataStorageService) { }
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-      const plants = this.plantService.
+      const plants = this.plantService.setPlants();
+      if(plants.length ==0){
+        return this.dataStorageService.fetchPlants();
+      } else {
+        return plants
+      }
       return this.dataStorageService.fetchBooks();
     }
 }
